f215cd10fcbc63c6733664fd78981297eafc2549
nigussolomon <nigussolomon444@gmail.com>
2022-10-01 13:00:38 +0300

    generated the customer model - see PC-17

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_01_054334) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_01_082025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_054334) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.bigint "contact_information_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "address_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["address_id"], name: "index_customers_on_address_id"
+    t.index ["contact_information_id"], name: "index_customers_on_contact_information_id"
+    t.index ["user_id"], name: "index_customers_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
@@ -43,4 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_01_054334) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "customers", "addresses"
+  add_foreign_key "customers", "contact_informations"
+  add_foreign_key "customers", "users"
 end
