122f037edeb260bd3531cb01cbcbbe7f1a0b2291
hawani99 <hawibek223@gmail.com>
2022-10-08 17:05:39 +0300

    added fields to product item - see PC-58

--- spec/models/product_item_spec.rb
+++ spec/models/product_item_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe ProductItem, type: :model do
   attributes = [
     { color: :presence },
-    { size: :presence },
+    { dimension: :presence },
     { price: :presence },
     { product: :belong_to },
     { currency: :belong_to },
     { unit_of_measure: :belong_to },
+    { material: :presence},
+    {supplier: :belong_to}
   ]
 
   include_examples('model_shared_spec', :product_item, attributes)
