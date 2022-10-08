5c51b1678bb7324a82a243ef5fe5d0b0e03c1b34
hawani99 <hawibek223@gmail.com>
2022-10-08 12:40:19 +0300

    removed unused models controllers migration files -see PC-55

--- app/models/address.rb
+++
@@ -1,3 +0,0 @@
-class Address < ApplicationRecord
-    validates :address_line1, :address_line2, :region, :city, :postal_code, presence: true
-end
--- app/models/contact_information.rb
+++
@@ -1,4 +0,0 @@
-class ContactInformation < ApplicationRecord
-    validates :primary_phone_number, :secondary_phone_number, :fax, :email, presence: true
-    validates :primary_phone_number, :secondary_phone_number, :email, uniqueness: true
-end
