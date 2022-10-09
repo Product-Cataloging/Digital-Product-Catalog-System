1f44cb02098b8116a2416e6bdff01d6f2b357173
nigussolomon <nigussolomon444@gmail.com>
2022-10-09 13:45:20 +0300

    added the search endpoint - see PC-65

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,43 +1,17 @@
 class SearchController < ApplicationController
-    # before_action :set_search
+    before_action :set_search
 
     def search
-        search_res = []
-        keyword = search_params['keyword'].downcase
-        res = Category.all.where("lower(name) LIKE :search", search: "#{keyword}")
-        res2 = Supplier.all.where("lower(company_name) LIKE :search", search: "#{keyword}")
-        res3 = Product.all.where("lower(name) LIKE :search", search: "#{keyword}")
-        
-        
-        for i in res
-            samaa = Product.where(category_id: i.id)
-            for y in samaa
-                if search_res.exclude?(y)
-                    search_res.push(y)
-                end
-            end
-        end
-
-        for i in res2
-            samaa2 = Product.where(supplier_id: i.id)
-            for y in samaa2
-                if search_res.exclude?(y)
-                    search_res.push(y)
-                end
-            end
-        end
-
-        for i in res3
-            if search_res.exclude?(i)
-                search_res.push(i)
-            end
-        end
-
-        render json: { success: true, data: search_res }
+        res = @service.search_prod()
+        render json: { success: true, data: serialize(res)}
     end
 
 
     private
+
+    def set_search
+        @service = SearchService.new(search: search_params[:keyword])
+    end
 
     def search_params
         params.require(:payload).permit(:keyword)
