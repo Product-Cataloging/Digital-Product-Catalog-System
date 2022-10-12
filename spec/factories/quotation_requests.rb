7f7d5d44a0657eb93f80d16ec713966e2ee0cc65
nigussolomon <nigussolomon444@gmail.com>
2022-10-13 00:29:21 +0300

    generated the quotation request model -see PC-66

---
+++ spec/factories/quotation_requests.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :quotation_request do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    phone_number { "MyString" }
+    email { "MyString" }
+    product_name { "MyString" }
+    image_url { "MyString" }
+    description { "MyString" }
+  end
+end
