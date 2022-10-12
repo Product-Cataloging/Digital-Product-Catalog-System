96a12241580868a11e0d53459a5a3518cb13b3f6
nigussolomon <nigussolomon444@gmail.com>
2022-10-13 01:27:55 +0300

    added the status field in notification -see PC-68

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -10,4 +10,6 @@ RSpec.describe Notification, type: :model do
   ]
 
   include_examples('model_shared_spec', :notification, attributes)
+
+  it { should validate_inclusion_of(:status).in_array(Notification::STATUS_TYPES) }
 end
