2e864916cb40fe0f9deafd40e8cc247b253f6d5a
nigussolomon <nigussolomon444@gmail.com>
2022-10-13 02:20:05 +0300

    notification is automatically created when a quotation is sent by a customer -see PC-70

--- app/controllers/quotation_requests_controller.rb
+++ app/controllers/quotation_requests_controller.rb
@@ -1,6 +1,16 @@
 class QuotationRequestsController < ApplicationController
     include Common
 
+    def create
+        model = QuotationRequest.new(model_params)
+        if model.save
+            service = NotificationService.new(first_name: model.first_name, last_name: model.last_name).create_notification()
+            render json: { success: true, data: serialize(model) }, status: :created
+        else
+            render json: { success: false, error: model.errors.full_messages[0] }, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def model_params
