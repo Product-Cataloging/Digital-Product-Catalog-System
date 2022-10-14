cc74ccac47982b854aede53ba8e0aac6c8e85b3f
hemensol <hemensol14@yahoo.com>
2022-10-14 04:28:11 +0300

    dropped unit of measure table -see PC-54

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_14_010300) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_012518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,13 +99,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_14_010300) do
     t.string "email", null: false
     t.string "postal_code"
     t.string "fax"
-  end
-
-  create_table "unit_of_measures", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "abbreviation", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
