d41f4a1bc08b521655c7569373c360980f08f8d9
hemensol <hemensol14@yahoo.com>
2022-10-14 04:15:39 +0300

    controller updated -see PC-54

--- app/controllers/product_items_controller.rb
+++ app/controllers/product_items_controller.rb
@@ -2,6 +2,6 @@ class ProductItemsController < ApplicationController
     include Common
 
     def model_params
-        params.require(:payload).permit(:color, :dimension, :unit_of_measure_id, :price, :product_id, :currency_id, :supplier_id, :material, :capacity, :status)
+        params.require(:payload).permit(:color, :dimension, :price, :product_id, :currency_id, :supplier_id, :material, :capacity, :status, :packaging_unit)
     end
 end
