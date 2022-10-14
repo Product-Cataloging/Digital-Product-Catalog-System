18fe4524b7fd80230e81bc43440e7cbd61cb3011
hemensol <hemensol14@yahoo.com>
2022-10-14 04:14:32 +0300

    model validation updated -see PC-54

--- app/models/product_item.rb
+++ app/models/product_item.rb
@@ -1,13 +1,11 @@
 class ProductItem < ApplicationRecord
-  belongs_to :unit_of_measure
   belongs_to :product
   belongs_to :currency
   belongs_to :supplier
 
   delegate(:company_name, to: :supplier, prefix: true)
-  delegate(:name, to: :unit_of_measure, prefix: true)
   delegate(:name, to: :currency, prefix: true)
   delegate(:name, to: :product, prefix: true)
 
-  validates :color, :price, :dimension, :material , presence: true
+  validates :color, :price, :dimension, :material, :packaging_unit , presence: true
 end
